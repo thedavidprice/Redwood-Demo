9276be00aa4a0966a2105552889194f1de0754ac
Rob Cameron <rob.cameron@fastmail.com>
2020-10-19 13:52:30 -0700

    Adds story decorator

--- web/src/components/BlogPost/BlogPost.stories.js
+++ web/src/components/BlogPost/BlogPost.stories.js
@@ -20,4 +20,9 @@ export const summary = () => {
   return <BlogPost post={POST} summary={true} />
 }
 
-export default { title: 'Components/BlogPost' }
+export default {
+  title: 'Components/BlogPost',
+  decorators: [
+    (Story) => <div className="m-8"><Story /></div>
+  ]
+}
