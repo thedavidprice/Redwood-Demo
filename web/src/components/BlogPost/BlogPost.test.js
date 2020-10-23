8c4769070ea6e409e4b56921cd145029a8601bba
Rob Cameron <rob.cameron@fastmail.com>
2020-10-23 09:57:39 -0700

    Test updates

--- web/src/components/BlogPost/BlogPost.test.js
+++ web/src/components/BlogPost/BlogPost.test.js
@@ -1,18 +1,30 @@
 import { render, screen } from '@redwoodjs/testing'
 
-import BlogPost from './BlogPost'
+import BlogPost, { SUMMARY_LENGTH, SUMMARY_SUFFIX } from './BlogPost'
+
+const POST = {
+  id: 1,
+  title: 'First post',
+  body: `Neutra tacos hot chicken prism raw denim, put a bird on it enamel pin post-ironic vape cred DIY. Street art next level umami squid. Hammock hexagon glossier 8-bit banjo. Neutra la croix mixtape echo park four loko semiotics kitsch forage chambray. Semiotics salvia selfies jianbing hella shaman. Letterpress helvetica vaporware cronut, shaman butcher YOLO poke fixie hoodie gentrify woke heirloom.`,
+  createdAt: new Date().toISOString(),
+}
 
 describe('BlogPost', () => {
   it('renders a blog post', () => {
-    const post = {
-      id: 1,
-      title: 'First post',
-      body: `Neutra tacos hot chicken prism raw denim, put a bird on it enamel pin post-ironic vape cred DIY. Street art next level umami squid. Hammock hexagon glossier 8-bit banjo. Neutra la croix mixtape echo park four loko semiotics kitsch forage chambray. Semiotics salvia selfies jianbing hella shaman. Letterpress helvetica vaporware cronut, shaman butcher YOLO poke fixie hoodie gentrify woke heirloom.`,
-      createdAt: new Date().toISOString(),
-    }
-    render(<BlogPost post={post} />)
+    render(<BlogPost post={POST} />)
 
-    expect(screen.getByText(post.title)).toBeInTheDocument()
-    expect(screen.getByText(post.body)).toBeInTheDocument()
+    expect(screen.getByText(POST.title)).toBeInTheDocument()
+    expect(screen.getByText(POST.body)).toBeInTheDocument()
+  })
+
+  it('renders a summary of a blog post', () => {
+    render(<BlogPost post={POST} summary={true} />)
+
+    expect(screen.getByText(POST.title)).toBeInTheDocument()
+    expect(
+      screen.getByText(
+        'Neutra tacos hot chicken prism raw denim, put a bird on it enamel pin post-ironic vape cred DIY. Str...'
+      )
+    ).toBeInTheDocument()
   })
 })
--- web/src/components/Comment/Comment.test.js
+++ web/src/components/Comment/Comment.test.js
@@ -1,11 +1,21 @@
-import { render } from '@redwoodjs/testing'
+import { render, screen } from '@redwoodjs/testing'
 
 import Comment from './Comment'
 
 describe('Comment', () => {
   it('renders successfully', () => {
-    // expect(() => {
-    //   render(<Comment />)
-    // }).not.toThrow()
+    const comment = {
+      name: 'John Doe',
+      body: 'This is my comment',
+      createdAt: '2020-01-02T12:34:56Z',
+    }
+
+    const { container } = render(<Comment comment={comment} />)
+
+    expect(screen.getByText(comment.name)).toBeInTheDocument()
+    expect(screen.getByText(comment.body)).toBeInTheDocument()
+    expect(
+      container.querySelector(`time[datetime="${comment.createdAt}"]`)
+    ).toBeInTheDocument()
   })
 })
