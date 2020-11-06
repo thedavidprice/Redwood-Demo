9b07ea6aaa36aaf293ea7966e87f4acf9a223aba
Rob Cameron <rob.cameron@fastmail.com>
2020-11-06 14:03:47 -0800

    Adds comments SDL and service

--- api/src/services/comments/comments.js
+++ api/src/services/comments/comments.js
@@ -4,6 +4,12 @@ export const comments = () => {
   return db.comment.findMany()
 }
 
+export const createComment = ({ input }) => {
+  return db.comment.create({
+    data: input,
+  })
+}
+
 export const Comment = {
   post: (_obj, { root }) =>
     db.comment.findOne({ where: { id: root.id } }).post(),
